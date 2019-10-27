8de267c3d30f2071b0726d5a6d4af7364bdc2269
Amgad Malik <amgadmalik@yahoo.com>
2019-10-27 00:36:08 +0000

    still working on scrapping the page for content'

--- lib/GreenvilleSchools/scraper.rb
+++ lib/GreenvilleSchools/scraper.rb
@@ -2,30 +2,32 @@ require 'open-uri'
 require 'pry'
 class GreenvilleSchools::Scraper
   
-  #url = "https://www.greatschools.org/north-carolina/greenville/schools/?gradeLevels%5B%5D=h&gradeLevels%5B%5D=m&gradeLevels%5B%5D=e"
-  
   def pagescrape
     
     url = "https://www.greatschools.org/north-carolina/greenville/schools/?gradeLevels%5B%5D=h&gradeLevels%5B%5D=m&gradeLevels%5B%5D=e"
     schools_list = Nokogiri::HTML(open(url))
     
     schools_array = []
-    
-    
-    schools_list.css("li.unsaved").each do |school|
-      name = school.css(".name").text
-      address = school.css(".address").text
-      school_url = school.css("a").attribute("href").value
-      scale = school.css(".scale").text
-      school_info = {:name => name,
-                :address => address,
-                :school_url => school_url, :scale => scale}
-      schools_array << school_info
-      end
-    schools_array
-    #binding.pry
+    list_array = schools_list.css("li.unsaved")
+    binding.pry
+    # schools_list.css("li.unsaved").each do |school|
+      list_array.each do |school|
+        binding.pry
+      end 
+    #   name = school.css(".name").text
+    #   address = school.css(".address").text
+    #   school_url = school.css("a").attribute("href").value
+    #   scale = school.css(".scale").text
+      
+    #   school_info = {:name => name,
+    #             :address => address,
+    #             :school_url => school_url, :scale => scale
+    #   }
+      
+    #   schools_array << school_info
+    #   end
+    # schools_array
 
-    puts "I am here"
   end
   
 end 
