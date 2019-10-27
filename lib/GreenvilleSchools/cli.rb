6b334c0edb5de15432157e39391ead8da3a7044e
Amgad Malik <amgadmalik@yahoo.com>
2019-10-27 20:37:27 +0000

    got listing schools by city to work. also second layer about school details.

--- lib/GreenvilleSchools/cli.rb
+++ lib/GreenvilleSchools/cli.rb
@@ -11,7 +11,6 @@ class GreenvilleSchools::CLI
       puts "To list all of the schools in area, enter 'list all'."
       puts "To list all of the schools by city, enter 'list by city'."
       puts "To list all of the schools by zip code, enter 'list by zip code'."
-      puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
@@ -30,7 +29,8 @@ class GreenvilleSchools::CLI
   end 
   
   def list_all
-
+    
+    input = ""
    #this give the user a list of the schools sorted by name alphabetically
    
     schools_sorted_by_name = GreenvilleSchools::School.all
@@ -38,7 +38,6 @@ class GreenvilleSchools::CLI
       puts "#{index}. #{school.name}"
     end
     puts "=============================="
-    #input = ""
   end
   
   def list_by_city
@@ -49,24 +48,29 @@ class GreenvilleSchools::CLI
     list_by_city.each.with_index(1) do |school,index|
     puts "#{index}. #{school.name}"
     end 
-    
+    puts "=============================="
+    school_details(list_by_city)
+  end 
+  
+  def school_details(list_of_schools) #this method gives the user detail info about the school they choose.
     input = ""
     while input != "back"
       puts "To get more info about one the schools listed, enter school's number."
       puts "To go back, type 'back'."
       puts "What would you like to do?"
       input = gets.strip
-      binding.pry
+      #binding.pry
       break if input == "back"
-        input = gets.strip.to_i
+        input = input.to_i
         
-        if (1..list_by_city.length).include?(input)
-          school = list_by_city[input-1]
+        if (1..list_of_schools.length).include?(input)
+          school = list_of_schools[input-1]
           puts school.name
           puts school.phone
+          puts "=============================="
         end
-      #end 
-      
     end 
   end 
+  
+  
 end
